0480541a919f1615128a8ab4194bcf30ab8618fb
Remy <shingiroremy303@gmail.com>
2025-09-24 21:48:14 +0200

    dfa

--- index.py
+++ index.py
@@ -5,4 +5,8 @@ while True:
     if choice == 'y':
         die1 = random.randint(1,6)
         die2 = random.randint(1,6)
-   
+    elif choice = 'n':
+        print('Thanks for playing the die game!!')
+        break
+    else:
+        print('Invalid choice')
