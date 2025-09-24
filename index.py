426187618bd56b63a339788c843c0f15375fed80
Remy <shingiroremy303@gmail.com>
2025-09-24 21:59:33 +0200

    debugged the issue

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ while True:
     if choice == 'y':
         die1 = random.randint(1,6)
         die2 = random.randint(1,6)
+        print(f'({die1},{die2})')
     elif choice == 'n':
         print('Thanks for playing the die game!!')
         break
