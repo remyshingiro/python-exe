2b34c650a49233f9b9bf331f500b8d1e46cb5380
Remy <shingiroremy303@gmail.com>
2025-09-24 21:49:53 +0200

    lowered the inputs

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
 import random
 
 while True:
-    choice = input('Wanna play die game? (y/n): ')
+    choice = input('Wanna play die game? (y/n): ').lower()
     if choice == 'y':
         die1 = random.randint(1,6)
         die2 = random.randint(1,6)
-    elif choice = 'n':
+    elif choice == 'n':
         print('Thanks for playing the die game!!')
         break
     else:
