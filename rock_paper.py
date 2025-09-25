df65f131690bd6099f43bdf139f8f960defcfe68
Remy <shingiroremy303@gmail.com>
2025-09-25 15:12:33 +0200

    added matching emojis

--- rock_paper.py
+++ rock_paper.py
@@ -1,4 +1,9 @@
+import random 
 
-user_choice = input('(r/p/s): ')
-try:
-    if user_choice
+choices = ('p','s','r')
+user_choice = input('rock, paper and scissors? (r/p/s): ').lower()
+if user_choice not in choices:
+    print('Invalid input.')
+computer_choice = random.choice(choices)
+print(f'You chose {user_choice}')
+print(f'Computer chose {computer_choice}')
