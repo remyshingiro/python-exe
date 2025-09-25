5cf280ede62d631d9e80922471d24989b72f1f43
Remy <shingiroremy303@gmail.com>
2025-09-25 15:13:11 +0200

    completed emojis dectionary

--- rock_paper.py
+++ rock_paper.py
@@ -1,9 +1,10 @@
 import random 
 
+emojis = {'r': '🪨', 'p': '📃', 's': '✂️'}
 choices = ('p','s','r')
 user_choice = input('rock, paper and scissors? (r/p/s): ').lower()
 if user_choice not in choices:
     print('Invalid input.')
 computer_choice = random.choice(choices)
-print(f'You chose {user_choice}')
-print(f'Computer chose {computer_choice}')
+print(f'You chose {emojis[user_choice]}')
+print(f'Computer chose {emojis[computer_choice]}')
