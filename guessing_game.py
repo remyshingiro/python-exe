b7589528ecbcafeb1d4844576b1032ac9810c5f0
Remy <shingiroremy303@gmail.com>
2025-09-25 12:01:30 +0200

    initialized the try except block

--- guessing_game.py
+++ guessing_game.py
@@ -1,3 +1,10 @@
 import random
 
 number_to_guess = random.randint(1, 100)
+while True:
+    try:
+        guess = int(input('Guess the number between 1 and 100: '))
+
+        if guess < number_to_guess:
+            print('Too low!!')
+        
